9e678205be50ce3ca84518ea0217e6d5f9dbf465
haifatanazefti <haifatanazefti97@gmail.com>
2023-03-29 15:39:01 +0200

    first commit

--- routes/filesRoutes.js
+++ routes/filesRoutes.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const multer = require('multer');
 const { 
-    getAllFiltedFilesByUserid, 
-    addFile1, 
-    addFile2, 
-    downloadFile, 
-    deleteFile, 
-    getOnefile,
-    deleteFileFromDownloadURLForMedecine,
+  
     deleteFileFromDownloadURLForPost} 
     = require("../controllers/fileController");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single('file');
 const router = express.Router();
-
-router.post("/sendfilee", upload, addFile1);
-router.post("/sendfile", upload, addFile2);
-router.get("/downoaldfile", downloadFile);
-router.get("/getfile/:name", getOnefile);
-router.get("/files/:uid", getAllFiltedFilesByUserid);
-router.delete("/deletefile/:fileName", deleteFile);
-router.delete("/file", deleteFileFromDownloadURLForMedecine);
 router.delete("/filepost", deleteFileFromDownloadURLForPost);
 module.exports = {
     routes: router
