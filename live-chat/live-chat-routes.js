2425c550982b4917d23d5b7591f313b2361685be
haifatanazefti <haifatanazefti97@gmail.com>
2023-06-06 17:52:49 +0200

    first commit

--- live-chat/live-chat-routes.js
+++ live-chat/live-chat-routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { getAllMessages, getCountMessages, getCountMessagesByUserId, hello } = require("./live-chat-controller");
+const { getAllMessages, getCountMessages, getCountMessagesByUserId, getAllUsersThatSendMsgForASpecificAssistant, addDiscussionAssUser, getAllMessagesByColleactionPath } = require("./live-chat-controller");
 const router = express.Router();
 router.get("/numberofmessage", getCountMessages);
 router.get("/msg/:idUser", getAllMessages);
 router.get("/nbr/:idUser", getCountMessagesByUserId);
-router.get("/hello", hello);
+
+router.post("/addiscuAssi/:assId/:cusId", addDiscussionAssUser)
+router.get("/listusersbyAssi/:assId", getAllUsersThatSendMsgForASpecificAssistant);
+router.get("/getMessages/:chatDocId/:discussionsDocId", getAllMessagesByColleactionPath);
 module.exports = {routes: router};
